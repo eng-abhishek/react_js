cb88a88036cd2ed3c7cf3abf1f8bd1aa2bb81fd2
Abhishek Soni <soni.abhishek@velocis.in>
2025-10-28 10:54:34 +0530

    manage code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import FormInput from './Components/Form';
 import Ref from './Components/Ref';
+import Ref2 from './Components/Ref2';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       {/* <FormInput /> */}
       <Ref />
+      <Ref2 />
     </div>
     </div>
     </div>
---
+++ src/Components/Ref2.js
@@ -0,0 +1,26 @@
+import React,{Component, createRef} from 'react';
+
+class Ref2 extends Component{
+
+    constructor(props){
+        super(props);
+        this.myRef = React.createRef();
+    }
+    
+    HandleSubmit = () => {
+    this.myRef.current.style.color ="red"; 
+    this.myRef.current.style.fontSize ="100px";
+    this.myRef.current.style.textAlign ="right";
+    console.log(this.myRef.current.innerHTML);
+    }
+
+    render(){
+     return (<>
+     <h1 ref={this.myRef}>Ram Ji </h1>
+     <button onClick={this.HandleSubmit}>Click Here...</button>
+     </>)
+    }
+
+}
+
+export default Ref2;
